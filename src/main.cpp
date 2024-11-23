613a43f4ab16e2ececbb987230aff8d035e62219
Julian <jdymail99@gmail.com>
2024-11-23 20:10:33 +0100

    debugging

--- src/main.cpp
+++ src/main.cpp
@@ -42,26 +42,24 @@ public:
 			// Process each offer in the 'offers' array
 			const auto &offersArray = body["offers"];
 			for (const auto &offerJson : offersArray) {
-				Offer offer ={
-						.start_date = offerJson["startDate"].get<int64_t>(),
-						.end_date = offerJson["endDate"].get<int64_t>(),
-						.id = offerJson["ID"].get<std::string>(),
-						.data =
-							[&offerJson] {
-								std::array<char, 256> arr{};
-								std::string data_str = offerJson["data"].get<std::string>();
-								std::copy(data_str.begin(), data_str.end(), arr.begin());
-								return arr;
-							}(),
-						.region_id = offerJson["mostSpecificRegionID"].get<int>(),
-						.number_seats = offerJson["numberSeats"].get<int>(),
-						.price = offerJson["price"].get<int>(),
-						.free_kilometers = offerJson["freeKilometers"].get<int>(),
-						.car_type = car_type_from_string(offerJson["carType"].get<std::string>()),
-						.has_vollkasko = offerJson["hasVollkasko"].get<bool>()
-					};
+				Offer offer = {.start_date = offerJson["startDate"].get<int64_t>(),
+							   .end_date = offerJson["endDate"].get<int64_t>(),
+							   .id = offerJson["ID"].get<std::string>(),
+							   .data =
+								   [&offerJson] {
+									   std::array<char, 256> arr{};
+									   std::string data_str = offerJson["data"].get<std::string>();
+									   std::copy(data_str.begin(), data_str.end(), arr.begin());
+									   return arr;
+								   }(),
+							   .region_id = offerJson["mostSpecificRegionID"].get<int>(),
+							   .number_seats = offerJson["numberSeats"].get<int>(),
+							   .price = offerJson["price"].get<int>(),
+							   .free_kilometers = offerJson["freeKilometers"].get<int>(),
+							   .car_type = car_type_from_string(offerJson["carType"].get<std::string>()),
+							   .has_vollkasko = offerJson["hasVollkasko"].get<bool>()};
 
-				std::cout << "New offer: "<<offer.id << std::endl;
+				std::cout << "New offer: " << offer.id << std::endl;
 				database.add_offer(offer);
 			}
 			response.send(Pistache::Http::Code::Ok, "Offers added successfully");
@@ -72,147 +70,136 @@ public:
 	}
 
 
-    // GET /api/offers - Adds new offers
-    void getOffers(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response) {
-        std::cout << "OffersHandler::getOffers" << std::endl;
+	// GET /api/offers - Adds new offers
+	void getOffers(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response) {
+		std::cout << "OffersHandler::getOffers" << std::endl;
 
-    try {
-    	std::cout << request.query().as_str() << std::endl;
-        // auto body = json::parse(request.body());
-        // Parse the required fields
-        // GetRequest get_request = {
-        //         .region_id = body.at("region_id").get<int>(),  // Required
-        //         .time_range_start = body.at("time_range_start").get<int64_t>(),  // Required
-        //         .time_range_end = body.at("time_range_end").get<int64_t>(),  // Required
-        //         .number_days = body.at("number_days").get<int>(),  // Required
-        //
-        //         .sort_order = body.value("sort_order", SortOrder::ASCENDING),  // Optional with default
-        //         .page = body.value("page", 1),  // Optional with default
-        //         .page_size = body.value("page_size", 10),  // Optional with default
-        //         .price_range_width = body.value("price_range_width", 1000),  // Optional with default
-        //         .min_free_kilometer_width = body.value("min_free_kilometer_width", 100),  // Optional with default
-        //
-        //         .min_number_seats = body.value("min_number_seats", 0),  // Optional
-        //         .min_price = body.value("min_price", 0),  // Optional
-        //         .max_price = body.value("max_price", 0),  // Optional
-        //         .car_type = body.value("car_type", CarType::ALL),  // Optional
-        //         .only_vollkasko = body.value("only_vollkasko", false),  // Optional
-        //         .min_free_kilometer = body.value("min_free_kilometer", 0)  // Optional
-        // };
+		try {
+			std::cout << request.query().as_str() << std::endl;
+			// auto body = json::parse(request.body());
+			// Parse the required fields
+			// GetRequest get_request = {
+			//         .region_id = body.at("region_id").get<int>(),  // Required
+			//         .time_range_start = body.at("time_range_start").get<int64_t>(),  // Required
+			//         .time_range_end = body.at("time_range_end").get<int64_t>(),  // Required
+			//         .number_days = body.at("number_days").get<int>(),  // Required
+			//
+			//         .sort_order = body.value("sort_order", SortOrder::ASCENDING),  // Optional with default
+			//         .page = body.value("page", 1),  // Optional with default
+			//         .page_size = body.value("page_size", 10),  // Optional with default
+			//         .price_range_width = body.value("price_range_width", 1000),  // Optional with default
+			//         .min_free_kilometer_width = body.value("min_free_kilometer_width", 100),  // Optional with
+			//         default
+			//
+			//         .min_number_seats = body.value("min_number_seats", 0),  // Optional
+			//         .min_price = body.value("min_price", 0),  // Optional
+			//         .max_price = body.value("max_price", 0),  // Optional
+			//         .car_type = body.value("car_type", CarType::ALL),  // Optional
+			//         .only_vollkasko = body.value("only_vollkasko", false),  // Optional
+			//         .min_free_kilometer = body.value("min_free_kilometer", 0)  // Optional
+			// };
 
-    	GetRequest get_request = {
-    		.region_id = std::stoi(request.query().get("regionID").value_or("-1")),
-			.time_range_start = std::stoll(request.query().get("timeRangeStart").value_or("0")),
-			.time_range_end = std::stoll(request.query().get("timeRangeEnd").value_or("0")),
-			.number_days = std::stoi(request.query().get("numberDays").value_or("0")),
+			GetRequest get_request = {
+				.region_id = std::stoi(request.query().get("regionID").value_or("-1")),
+				.time_range_start = std::stoll(request.query().get("timeRangeStart").value_or("0")),
+				.time_range_end = std::stoll(request.query().get("timeRangeEnd").value_or("0")),
+				.number_days = std::stoi(request.query().get("numberDays").value_or("0")),
 
-			.sort_order = request.query().get("sortOrder").value_or("price-asc") == "price-asc"
-						  ? SortOrder::ASCENDING
-						  : SortOrder::DESCENDING,
-			.page = std::stoi(request.query().get("page").value_or("1")),
-			.page_size = std::stoi(request.query().get("pageSize").value_or("10")),
-			.price_range_width = std::stoi(request.query().get("priceRangeWidth").value_or("1000")),
-			.min_free_kilometer_width = std::stoi(request.query().get("minFreeKilometerWidth").value_or("100")),
+				.sort_order = request.query().get("sortOrder").value_or("price-asc") == "price-asc"
+					? SortOrder::ASCENDING
+					: SortOrder::DESCENDING,
+				.page = std::stoi(request.query().get("page").value_or("1")),
+				.page_size = std::stoi(request.query().get("pageSize").value_or("10")),
+				.price_range_width = std::stoi(request.query().get("priceRangeWidth").value_or("1000")),
+				.min_free_kilometer_width = std::stoi(request.query().get("minFreeKilometerWidth").value_or("100")),
 
-			.min_number_seats = std::stoi(request.query().get("minNumberSeats").value_or("0")),
-			.min_price = std::stoi(request.query().get("minPrice").value_or("0")),
-			.max_price = std::stoi(request.query().get("maxPrice").value_or("0")),
-			.car_type = car_type_from_string(request.query().get("carType").value_or("all")),
-			.only_vollkasko = request.query().get("onlyVollkasko").value_or("false") == "true",
-			.min_free_kilometer = std::stoi(request.query().get("minFreeKilometer").value_or("0"))
-		};
+				.min_number_seats = std::stoi(request.query().get("minNumberSeats").value_or("0")),
+				.min_price = std::stoi(request.query().get("minPrice").value_or("0")),
+				.max_price = std::stoi(request.query().get("maxPrice").value_or("0")),
+				.car_type = car_type_from_string(request.query().get("carType").value_or("all")),
+				.only_vollkasko = request.query().get("onlyVollkasko").value_or("false") == "true",
+				.min_free_kilometer = std::stoi(request.query().get("minFreeKilometer").value_or("0"))};
 
-    	std::cout << "OFFER" << std::endl;
+			std::cout << "OFFER" << std::endl;
 
-        Offers offers = database.get(get_request);
+			Offers offers = database.get(get_request);
 
-        // Convert the Offers object to JSON
-        json response_json = {
-                {"offers", json::array()},
-                {"price_ranges", json::array()},
-                {"car_type_counts", {
-                                   {"small_count", offers.car_type_counts.small_count},
-                                   {"sports_count", offers.car_type_counts.sports_count},
-                                   {"luxury_count", offers.car_type_counts.luxury_count},
-                                   {"family_count", offers.car_type_counts.family_count}
-                           }},
-                {"seat_counts", json::array()},
-                {"free_kilometer_ranges", json::array()},
-                {"voll_kasko_count", {
-                                   {"vollkasko_true_count", offers.voll_kasko_count.vollkasko_true_count},
-                                   {"vollkasko_false_count", offers.voll_kasko_count.vollkasko_false_count}
-                           }}
-        };
+			std::cout << "Json die nullte" << offers.offers.size() << std::endl;
 
-    	std::cout << "Json die erste" << std::endl;
+			// Convert the Offers object to JSON
+			json response_json = {{"offers", json::array()},
+								  {"price_ranges", json::array()},
+								  {"car_type_counts",
+								   {{"small_count", offers.car_type_counts.small_count},
+									{"sports_count", offers.car_type_counts.sports_count},
+									{"luxury_count", offers.car_type_counts.luxury_count},
+									{"family_count", offers.car_type_counts.family_count}}},
+								  {"seat_counts", json::array()},
+								  {"free_kilometer_ranges", json::array()},
+								  {"voll_kasko_count",
+								   {{"vollkasko_true_count", offers.voll_kasko_count.vollkasko_true_count},
+									{"vollkasko_false_count", offers.voll_kasko_count.vollkasko_false_count}}}};
 
-        // Serialize the `offers` field
-        for (const auto& offer : offers.offers) {
-            response_json["offers"].push_back({
-                                                      {"id", offer.id},
-                                                      {"data", std::string(offer.data.begin(), offer.data.end())}  // Convert array<char, 256> to string
-                                              });
-        }
+			std::cout << "Json die erste" << std::endl;
 
-        // Serialize the `price_ranges` field
-        for (const auto& range : offers.price_ranges) {
-            response_json["price_ranges"].push_back({
-                                                            {"price_range_start", range.price_range_start},
-                                                            {"price_range_end", range.price_range_end},
-                                                            {"price_range_count", range.price_range_count}
-                                                    });
-        }
+			// Serialize the `offers` field
+			for (const auto &offer : offers.offers) {
+				response_json["offers"].push_back({
+					{"id", offer.id},
+					{"data", std::string(offer.data.begin(), offer.data.end())} // Convert array<char, 256> to string
+				});
+			}
 
-        // Serialize the `seat_counts` field
-        for (const auto& seat : offers.seat_counts) {
-            response_json["seat_counts"].push_back({
-                                                           {"number_seats", seat.number_seats},
-                                                           {"count", seat.count}
-                                                   });
-        }
+			// Serialize the `price_ranges` field
+			for (const auto &range : offers.price_ranges) {
+				response_json["price_ranges"].push_back({{"price_range_start", range.price_range_start},
+														 {"price_range_end", range.price_range_end},
+														 {"price_range_count", range.price_range_count}});
+			}
 
-        // Serialize the `free_kilometer_ranges` field
-        for (const auto& range : offers.free_kilometer_ranges) {
-            response_json["free_kilometer_ranges"].push_back({
-                                                                     {"free_kilometer_range_start", range.free_kilometer_range_start},
-                                                                     {"free_kilometer_range_end", range.free_kilometer_range_end},
-                                                                     {"free_kilometer_range_count", range.free_kilometer_range_count}
-                                                             });
-        }
+			// Serialize the `seat_counts` field
+			for (const auto &seat : offers.seat_counts) {
+				response_json["seat_counts"].push_back({{"number_seats", seat.number_seats}, {"count", seat.count}});
+			}
 
-    	std::cout << "json die letzte" << std::endl;
+			// Serialize the `free_kilometer_ranges` field
+			for (const auto &range : offers.free_kilometer_ranges) {
+				response_json["free_kilometer_ranges"].push_back(
+					{{"free_kilometer_range_start", range.free_kilometer_range_start},
+					 {"free_kilometer_range_end", range.free_kilometer_range_end},
+					 {"free_kilometer_range_count", range.free_kilometer_range_count}});
+			}
+
+			std::cout << "json die letzte" << std::endl;
 
 
-        // Send the JSON response
-        response.send(Pistache::Http::Code::Ok, response_json.dump());
+			// Send the JSON response
+			response.send(Pistache::Http::Code::Ok, response_json.dump());
 
-    	std::cout << "Fertig" << std::endl;
+			std::cout << "Fertig" << std::endl;
+		}
+		catch (const json::exception &e) {
+			std::cout << "CATCH" << e.what() << std::endl;
 
-    }
-    catch (const json::exception& e) {
-    	std::cout << "CATCH" << e.what() << std::endl;
-
-        // Handle JSON parsing errors or missing fields
-        response.send(Pistache::Http::Code::Bad_Request, std::string("Error parsing request: ") + e.what());
-    }
+			// Handle JSON parsing errors or missing fields
+			response.send(Pistache::Http::Code::Bad_Request, std::string("Error parsing request: ") + e.what());
+		}
 		std::cout << "Fertig x 2" << std::endl;
-
-    }
+	}
 
 
 	// DELETE /api/offers - Clears all offers
-	void deleteOffers(const Pistache::Rest::Request& request, Pistache::Http::ResponseWriter response) {
+	void deleteOffers(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response) {
 		std::cout << "deleteOffers" << std::endl;
 		try {
 			// database.clear_offers(); // Assuming the database has a method to clear all offers
 			std::cout << "All offers cleared from the database" << std::endl;
 			response.send(Pistache::Http::Code::Ok, "Data was cleaned up");
 		}
-		catch (const std::exception& e) {
+		catch (const std::exception &e) {
 			response.send(Pistache::Http::Code::Internal_Server_Error, e.what());
 		}
 	}
-
 
 
 private:
@@ -220,11 +207,11 @@ private:
 };
 
 
-void setupRoutes(Pistache::Rest::Router &router, OffersHandler& offersHandler) {
+void setupRoutes(Pistache::Rest::Router &router, OffersHandler &offersHandler) {
 	using namespace Pistache::Rest;
 	Routes::Post(router, "/api/offers", Routes::bind(&OffersHandler::postOffers, &offersHandler));
 	Routes::Delete(router, "/api/offers", Routes::bind(&OffersHandler::deleteOffers, &offersHandler));
-    Routes::Get(router, "/api/offers", Routes::bind(&OffersHandler::getOffers, &offersHandler));
+	Routes::Get(router, "/api/offers", Routes::bind(&OffersHandler::getOffers, &offersHandler));
 }
 
 int main() {
@@ -237,9 +224,9 @@ int main() {
 	Pistache::Rest::Router router;
 	setupRoutes(router, offersHandler);
 
-    auto options = Pistache::Http::Endpoint::options()
-    .threads(1);
-    // .flags(Pistache::Tcp::Options::ReuseAddr).maxRequestSize(1 * 1024 * 1024);  // Allow 100 MB payloads (you can adjust this as needed);
+	auto options = Pistache::Http::Endpoint::options().threads(1);
+	// .flags(Pistache::Tcp::Options::ReuseAddr).maxRequestSize(1 * 1024 * 1024);  // Allow 100 MB payloads (you can
+	// adjust this as needed);
 
 	server.init(options);
 
