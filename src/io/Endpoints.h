b0fe1e1c4c6da35f5d2c6032064f3cd159a939de
Julian <jdymail99@gmail.com>
2024-11-23 22:08:58 +0100

    debugging

--- src/io/Endpoints.h
+++ src/io/Endpoints.h
@@ -1,16 +1,17 @@
-// #pragma once
-// #include <pistache/endpoint.h>
-// #include <pistache/http.h>
-// #include <pistache/router.h>
-// #include "src/Database.h"
-//
-// namespace db::io {
-// 	class OffersHandler {
-// 	public:
-// 		OffersHandler(db::DataBase & db) : database(db) {}
-// 		void postOffers(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response);
-// 		void getOffers(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response);
-// 	private:
-// 		DataBase & database;
-// 	};
-// }
+#pragma once
+#include <pistache/http.h>
+#include <pistache/router.h>
+
+#include "database/DataBase.h"
+
+namespace db::io {
+	class OffersHandler {
+	public:
+		explicit OffersHandler(db::DataBase & db) : database(db) {}
+		void postOffers(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response);
+		void getOffers(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response);
+	private:
+		Offer read_offer();
+		DataBase & database;
+	};
+}
