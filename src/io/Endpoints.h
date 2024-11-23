55e54da1b4cb40cee78b54f5b4c4976c589f2a8a
Julian <jdymail99@gmail.com>
2024-11-23 21:58:53 +0100

    debugging

---
+++ src/io/Endpoints.h
@@ -0,0 +1,14 @@
+// #pragma once
+// #include <pistache/endpoint.h>
+// #include <pistache/http.h>
+// #include <pistache/router.h>
+//
+// namespace db::io {
+// 	class OffersHandler {
+// 	public:
+// 		OffersHandler(db::DataBase & db) : database(db) {}
+// 		void postOffers(const Pistache::Rest::Request &request, Pistache::Http::ResponseWriter response);
+//
+//
+// 	}
+// }
